cce3a117dc7847366430e9e0b489bbee9b69fa65
Jesus <Jesus@Mafia>
2018-06-20 23:29:10 +0200

    1

--- erlang2/src/strings.erl
+++ erlang2/src/strings.erl
@@ -9,9 +9,17 @@
 -export([start/0]). 
 
 start() ->
-   Str1 = "This is a string", 
-   io:fwrite("~p~n",[Str1]).
-
+   %Str1 = "This is a string",
+   %Str2 = " cadena2",
+   %Str3 = " weeee",
+   %io:fwrite("~p~n",[Str1 ++ Str2 ++ Str3 ++ " holaaaa"]).
+{_, Nombre} = io:read("Dimetunombre: "),
+{_, Apellidos} = io:read("Dimetusapellidos: "),
+io:fwrite("~w~n", [Nombre]),
+io:fwrite("~p~n",[Apellidos]).
+%{ok, Term} = io:read("Enter a number: "),
+        %io:format("The number you entered plus one is: ~w~n", 
+                 % [Term + 1]).
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
